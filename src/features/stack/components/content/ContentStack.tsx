957aa72f6e11a81ae8dca57c464d19eed86d62fc
Vicente <vicen.dark131@gmail.com>
2026-06-17 17:58:12 -0600

    feat(stack): agregar componente Title con texto Stack

--- src/features/stack/components/content/ContentStack.tsx
+++ src/features/stack/components/content/ContentStack.tsx
@@ -1,6 +1,9 @@
+import { Title } from "@/features/stack/components/title";
+
 export function ContentStack() {
   return (
     <section id="stack" className="relative flex flex-col py-10 overflow-hidden">
+      <Title />
     </section>
   );
 }
---
+++ src/features/stack/components/title/Title.tsx
@@ -0,0 +1,7 @@
+export function Title() {
+  return (
+    <h2 className="font-comic text-4xl md:text-5xl tracking-wide text-center">
+      Stack
+    </h2>
+  );
+}
