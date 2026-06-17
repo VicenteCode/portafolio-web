a79173dc4dc844e992d955e1edd54eaf920e85f7
Vicente <vicen.dark131@gmail.com>
2026-06-17 13:04:05 -0600

    Agregando la carta de sobre mi

--- src/features/about-me/components/content/ContentAboutMe.tsx
+++ src/features/about-me/components/content/ContentAboutMe.tsx
@@ -1,9 +1,11 @@
 import { Title } from "@/features/about-me/components/title";
+import { AboutCard } from "@/features/about-me/components/card";
 
 export function ContentAboutMe() {
   return (
     <section id="about-me">
       <Title />
+      <AboutCard />
     </section>
   );
 }
