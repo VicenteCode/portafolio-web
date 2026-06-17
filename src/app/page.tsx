42061184244077ed2e91e3d48c8efe21c9c4b224
Vicente <vicen.dark131@gmail.com>
2026-06-17 17:56:32 -0600

    feat(stack): agregar ContentStack a la página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import { Layout } from "@/components/layout/Layout";
 import { ContentAboutMe } from "@/features/about-me/components/content";
+import { ContentStack } from "@/features/stack/components/content";
 import { NavigationBar } from "@/features/navigation/components/NavigationBar";
 import { Hero } from "@/features/start/components/hero";
-
 
 export default function Page() {
   return (
     <Layout navbar={<NavigationBar />}>
       <Hero />
       <ContentAboutMe />
+      <ContentStack />
     </Layout>
   );
 }
