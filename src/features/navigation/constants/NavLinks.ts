d2af70fb5277d817c71e32ce4aacefd836b10736
Vicente <vicen.dark131@gmail.com>
2026-06-17 12:16:06 -0600

    refactor(navigation): reordenar enlaces del navbar

--- src/features/navigation/constants/NavLinks.ts
+++ src/features/navigation/constants/NavLinks.ts
@@ -2,8 +2,8 @@ import { User, Briefcase, GraduationCap, Wrench, Code } from 'lucide-react'
 
 export const NavLinks = [
     { label: 'About Me', icon: User, href: '#about' },
-    { label: 'Experience', icon: Briefcase, href: '#experience'},
-    { label: 'Education', icon: GraduationCap, href: '#education'},
-    { label: 'Skills', icon: Wrench, href: '#skills'},
     { label: 'Stack', icon: Code, href: '#stack'},
+    { label: 'Experience', icon: Briefcase, href: '#experience'},
+    { label: 'Skills', icon: Wrench, href: '#skills'},
+    { label: 'Education', icon: GraduationCap, href: '#education'},
 ]
