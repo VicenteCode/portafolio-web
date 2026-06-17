a34fa0d62c16324f3451c1de27d418c45d0f642b
Vicente <vicen.dark131@gmail.com>
2026-06-17 13:34:08 -0600

    feat(about-me): agregar Moon al lado de la AboutCard

--- src/features/about-me/components/content/ContentAboutMe.tsx
+++ src/features/about-me/components/content/ContentAboutMe.tsx
@@ -1,12 +1,14 @@
 import { Title } from "@/features/about-me/components/title";
 import { AboutCard } from "@/features/about-me/components/card";
+import { Moon } from "@/features/about-me/components/animation";
 
 export function ContentAboutMe() {
   return (
     <section id="about-me" className="flex flex-col py-20">
       <Title />
-      <div className="mt-16 self-start">
+      <div className="mt-16 flex flex-col lg:flex-row items-center gap-12">
         <AboutCard />
+        <Moon />
       </div>
     </section>
   );
