a87a9c4e8edae5bf4dd5e8504a019c63eabaccbe
Vicente <vicen.dark131@gmail.com>
2026-06-17 18:58:51 -0600

    Primero va la experiencia y luego el stack

--- src/features/navigation/constants/NavLinks.ts
+++ src/features/navigation/constants/NavLinks.ts
@@ -3,8 +3,8 @@ import { User, Briefcase, GraduationCap, Wrench, Code, Home } from 'lucide-react
 export const NavLinks = [
     { label: 'Start', icon: Home, href: '#start'},
     { label: 'About Me', icon: User, href: '#about-me' },
-    { label: 'Stack', icon: Code, href: '#stack'},
     { label: 'Experience', icon: Briefcase, href: '#experience'},
+    { label: 'Stack', icon: Code, href: '#stack'},
     { label: 'Skills', icon: Wrench, href: '#skills'},
     { label: 'Education', icon: GraduationCap, href: '#education'},
 ]
