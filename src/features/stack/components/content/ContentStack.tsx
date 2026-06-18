140261d5c29080010640a1568d2769a3dff4a08d
Vicente <vicen.dark131@gmail.com>
2026-06-17 18:02:38 -0600

    Cambiando el nombre del titulo

--- src/features/stack/components/content/ContentStack.tsx
+++ src/features/stack/components/content/ContentStack.tsx
@@ -3,7 +3,7 @@ import { SectionTitle } from "@/components/ui/section-title";
 export function ContentStack() {
   return (
     <section id="stack" className="relative flex flex-col py-10 overflow-hidden">
-      <SectionTitle text="Stack" />
+      <SectionTitle text="My Stack" />
     </section>
   );
 }
