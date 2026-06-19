b70995e04dbecfb47ed7cdc00e2e0e4a6ff09a54
Vicente <vicen.dark131@gmail.com>
2026-06-19 16:44:36 -0600

    Correciones

--- src/features/navigation/constants/NavLinks.ts
+++ src/features/navigation/constants/NavLinks.ts
@@ -1,6 +1,6 @@
 import { User, Briefcase, GraduationCap, Wrench, Code, Home } from 'lucide-react'
 
-export const NAV_ITEMS = [
+export const NAV_LINKS = [
   { key: 'start' as const, icon: Home, href: '#start' },
   { key: 'aboutMe' as const, icon: User, href: '#about-me' },
   { key: 'experience' as const, icon: Briefcase, href: '#experience' },
