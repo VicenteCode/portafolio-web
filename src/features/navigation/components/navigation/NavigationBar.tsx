f6e7a31d4321b1b3de15f661ff281c257ac638c5
Vicente <vicen.dark131@gmail.com>
2026-06-19 16:42:38 -0600

    fix: corrige referencias a NAV_LINKS en NavigationBar

--- src/features/navigation/components/navigation/NavigationBar.tsx
+++ src/features/navigation/components/navigation/NavigationBar.tsx
@@ -1,8 +1,17 @@
 "use client";
 
 import { Navbar } from "@/components/layout/Navbar";
-import { NavLinks } from "../../constants/NavLinks";
+import { NAV_LINKS } from "../../constants/NavLinks";
+import { useLanguage } from "@/lib/language/LanguageContext";
+import { translations } from "@/lib/language/translations";
 
 export function NavigationBar() {
-  return <Navbar links={NavLinks} />;
+  const { lang } = useLanguage();
+  const t = translations[lang].nav;
+  const links = NAV_LINKS.map(({ key, href, icon }) => ({
+    label: t[key],
+    href,
+    icon,
+  }));
+  return <Navbar links={links} />;
 }
