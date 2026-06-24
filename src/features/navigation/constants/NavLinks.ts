363660b4893f4bb8ace0362bc6516d1f566ea84a
Vicente <vicen.dark131@gmail.com>
2026-06-24 14:31:11 -0600

    Agregando el nuevo link de proyecto a mi NavBar

--- src/features/navigation/constants/NavLinks.ts
+++ src/features/navigation/constants/NavLinks.ts
@@ -4,6 +4,7 @@ export const NAV_LINKS = [
   { key: 'start' as const, icon: Home, href: '#start' },
   { key: 'aboutMe' as const, icon: User, href: '#about-me' },
   { key: 'experience' as const, icon: Briefcase, href: '#experience' },
+  { key: 'project' as const, icon: Code, href: '#project' },
   { key: 'stack' as const, icon: Code, href: '#stack' },
   { key: 'skills' as const, icon: Wrench, href: '#skills' },
   { key: 'education' as const, icon: GraduationCap, href: '#education' },
