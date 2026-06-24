1965f79232f40ffa9b536bb00bbe7cbe83eb7f32
Vicente <vicen.dark131@gmail.com>
2026-06-24 14:34:35 -0600

    Cambiando el icono de mi proyectos por algo más coherente

--- src/features/navigation/constants/NavLinks.ts
+++ src/features/navigation/constants/NavLinks.ts
@@ -1,10 +1,10 @@
-import { User, Briefcase, GraduationCap, Wrench, Code, Home } from 'lucide-react'
+import { User, Briefcase, GraduationCap, Wrench, Code, Home, FolderKanban } from 'lucide-react'
 
 export const NAV_LINKS = [
   { key: 'start' as const, icon: Home, href: '#start' },
   { key: 'aboutMe' as const, icon: User, href: '#about-me' },
   { key: 'experience' as const, icon: Briefcase, href: '#experience' },
-  { key: 'project' as const, icon: Code, href: '#project' },
+  { key: 'project' as const, icon: FolderKanban, href: '#project' },
   { key: 'stack' as const, icon: Code, href: '#stack' },
   { key: 'skills' as const, icon: Wrench, href: '#skills' },
   { key: 'education' as const, icon: GraduationCap, href: '#education' },
